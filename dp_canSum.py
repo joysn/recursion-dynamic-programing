84c297c03ffb767949ad0e1b9bd12e33aa5e8ff7
joysn <joysn1980@yahoo.com>
2022-01-07 17:53:31 +0530

    Add files via upload

--- dp_canSum.py
+++ dp_canSum.py
@@ -1,5 +1,5 @@
-
 def canSum(target,numbers):
+
 	if target in memo.keys():
 		return memo[target]
 
@@ -16,8 +16,34 @@ def canSum(target,numbers):
 	memo[target] = False
 	return False
 
+def canSum_bottomup(target,numbers):
+	tab = [False for i in range(target+1)]
 
+	tab[0] = True
+
+	for i in range(target+1):
+		if tab[i] == True:
+			for num in numbers:
+				if i+num <= target:
+					tab[i+num] = True
+					if i+num == target:
+						return tab[i+num]
+
+	return tab[target]
+
+# m = target
+# n = number of numbers
+# Brute
+# Time = O(n^m)
+# Space = O(m)
+# Memo
+# Time = O(n*m)
+# Space = O(m)
+# Tabular
+# Time = O(n*m)
+# Space = O(n)
 if __name__ == "__main__":
+
 	memo = dict()
 	count = 0
 	print("True",canSum(7,[4,5,3,7]),count)
@@ -47,3 +73,25 @@ if __name__ == "__main__":
 	# 	memo = dict()
 	# 	count = 0
 	# 	print(i,canSum(i,[4,3,7]),count)
+
+	print("True",canSum_bottomup(7,[4,5,3,7]))
+	print("False",canSum_bottomup(7,[4,5]))
+	print("True",canSum_bottomup(7,[2,3]))
+	print("False",canSum_bottomup(7,[4,2]))
+	print("True",canSum_bottomup(8,[2,3,5]))
+	print("False",canSum_bottomup(300,[7,14]))
+
+
+# (base) coding % python canSum.py 
+# True True 0
+# False False 0
+# True True 0
+# False False 0
+# True True 0
+# False False 0
+# True True
+# False False
+# True True
+# False False
+# True True
+# False False
