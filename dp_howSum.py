7741a8a5cc04d849fd5ed0675252d845f5b98af5
joysn <joysn1980@yahoo.com>
2022-01-08 09:47:27 +0530

    Add files via upload

--- dp_howSum.py
+++ dp_howSum.py
@@ -1,4 +1,6 @@
+import copy
 def howSum(target,numbers):
+
 	if target in memo.keys():
 		return memo[target]
 
@@ -17,6 +19,22 @@ def howSum(target,numbers):
 	memo[target] = None
 	return None
 
+def howSum_bottomup(target,numbers):
+
+	tab = [None for i in range(target+1)]
+	
+	tab[0] = []
+	for i in range(target+1):
+		if tab[i] is not None:
+			for num in numbers:
+				if i+num <= target:
+					tab[i+num] = copy.deepcopy(tab[i])
+					tab[i+num].append(num)
+					if i+num == target:
+						return tab[i+num]
+
+	return tab[target]
+
 # n = len(numbers)
 # m = targetSum
 
@@ -28,37 +46,58 @@ def howSum(target,numbers):
 # Time = O(n*m)  
 # Space = O(m*m)
 
+# Bottom Up
+# Time = O(n*m*m) - last m is due to copying m array everytime
+# Space = O(m*m) - last m is due to time required to copy m sized array
+
 if __name__ == "__main__":
 
 	memo = dict()
-	count = 0
 	print("7,[4,5,3,7] True",howSum(7,[4,5,3,7]))
 
 	memo = dict()
-	count = 0
 	print("7,[4,5] False",howSum(7,[4,5]))
 
 	memo = dict()
-	count = 0
 	print("7,[2,3] True",howSum(7,[2,3]))
 
 	memo = dict()
-	count = 0
 	print("7,[4,2] False",howSum(7,[4,2]))
 
 	memo = dict()
-	count = 0
 	print("8,[2,3,5] True",howSum(8,[2,3,5]))
 
 	memo = dict()
-	count = 0
 	print("0,[2,3,5] False",howSum(0,[2,3,5]))
 
 	memo = dict()
-	count = 0
 	print("300,[7,14] False",howSum(300,[7,14]))
 
 	# for i in range(1,50):
 	# 	memo = dict()
 	# 	count = 0
 	# 	print(i,howSum(i,[4,3,7]),count)
+
+	print("7,[4,5,3,7] True",howSum_bottomup(7,[4,5,3,7]))
+	print("7,[4,5] False",howSum_bottomup(7,[4,5]))
+	print("7,[2,3] True",howSum_bottomup(7,[2,3]))
+	print("7,[4,2] False",howSum_bottomup(7,[4,2]))
+	print("8,[2,3,5] True",howSum_bottomup(8,[2,3,5]))
+	print("0,[2,3,5] False",howSum_bottomup(0,[2,3,5]))
+	print("300,[7,14] False",howSum_bottomup(300,[7,14]))
+
+# (base) coding % python howSum.py 
+# 7,[4,5,3,7] True [3, 4]
+# 7,[4,5] False None
+# 7,[2,3] True [3, 2, 2]
+# 7,[4,2] False None
+# 8,[2,3,5] True [2, 2, 2, 2]
+# 0,[2,3,5] False []
+# 300,[7,14] False None
+# 7,[4,5,3,7] True [7]
+# 7,[4,5] False None
+# 7,[2,3] True [2, 2, 3]
+# 7,[4,2] False None
+# 8,[2,3,5] True [3, 5]
+# 0,[2,3,5] False []
+# 300,[7,14] False None
